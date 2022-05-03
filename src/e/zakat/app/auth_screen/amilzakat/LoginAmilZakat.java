dfe1abd573d28a3ad0d22c2b5c218134d469b41a
riyandifirman <riyandi.firman08@gmail.com>
2022-05-04 02:07:35 +0700

    Solving import package reference to HomePageMuzakki

--- src/e/zakat/app/auth_screen/amilzakat/LoginAmilZakat.java
+++ src/e/zakat/app/auth_screen/amilzakat/LoginAmilZakat.java
@@ -6,7 +6,7 @@ package e.zakat.app.auth_screen.amilzakat;
 
 import e.zakat.app.auth_screen.muzakki.RegisterMuzakki;
 import e.zakat.app.initial_screen.ChooseRoles;
-import e.zakat.app.muzakki_features.HomePageMuzakki;
+import e.zakat.app.muzakki_features.maal.HomePageMuzakki;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.sql.*;  
