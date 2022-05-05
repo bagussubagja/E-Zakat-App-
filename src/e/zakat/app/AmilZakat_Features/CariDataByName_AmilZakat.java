446d9ae96a3b4e6bc25c439f5ae3142311334ea8
bagussubagja <bagussubagja@upi.edu>
2022-05-05 08:06:01 +0700

    add CariNamaResult_AmilZakat Screen

--- src/e/zakat/app/AmilZakat_Features/CariDataByName_AmilZakat.java
+++ src/e/zakat/app/AmilZakat_Features/CariDataByName_AmilZakat.java
@@ -12,12 +12,12 @@ import javax.swing.ImageIcon;
  *
  * @author bagus
  */
-public class CariNamaByName_AmilZakat extends javax.swing.JFrame {
+public class CariDataByName_AmilZakat extends javax.swing.JFrame {
 
     /**
      * Creates new form CariNamaByName_AmilZakat
      */
-    public CariNamaByName_AmilZakat() {
+    public CariDataByName_AmilZakat() {
         initComponents();
          ImageIcon myimage = new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/assets/carinama.png")));
     
@@ -160,20 +160,21 @@ public class CariNamaByName_AmilZakat extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CariNamaByName_AmilZakat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CariDataByName_AmilZakat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CariNamaByName_AmilZakat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CariDataByName_AmilZakat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CariNamaByName_AmilZakat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CariDataByName_AmilZakat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CariNamaByName_AmilZakat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CariDataByName_AmilZakat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new CariNamaByName_AmilZakat().setVisible(true);
+                new CariDataByName_AmilZakat().setVisible(true);
             }
         });
     }
